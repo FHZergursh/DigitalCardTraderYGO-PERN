0ac5d4b19fe5c7e41aa9a70ffb76111a8c052ecc
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-06-01 22:01:19 +0100

    apis part 1
    get is done, working on create

--- backend/src/routes/monsterCardRoute.js
+++ backend/src/routes/monsterCardRoute.js
@@ -1,5 +1,9 @@
 import express from "express"
+import { createMonsterCard, getAllMonsterCards } from "../controllers/monsterCardController.js";
 
 const monsterRouter = express.Router();
+
+monsterRouter.get("/", getAllMonsterCards)
+monsterRouter.post("/", createMonsterCard)
 
 export default monsterRouter
