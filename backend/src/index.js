cf2a62da1489812634016d5dc25a1303c3a35e49
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-06-01 20:59:10 +0100

    setup db for monster cards
    title

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import express from "express"
+import 'dotenv/config'
+import { setupDB } from "./db/db.js";
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
+
+setupDB().then(
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+}));
---
+++ backend/src/routes/monsterCardRoute.js
@@ -0,0 +1,5 @@
+import express from "express"
+
+const monsterRouter = express.Router();
+
+export default monsterRouter
